bc14f0cf45f7b7824c9a79d50e1735035a1e0e3f
aillet <aillet.gabriel05@gmail.com>
2022-11-22 13:29:20 +0100

    'd'

--- src/App.js
+++ src/App.js
@@ -2,92 +2,112 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  
   const shapes = document.getElementsByClassName("shape");
-  const colors = ["#84e3c8",'#a8e6cf',"#dcedc1",'#ffd3b6','#ffaaa5','#ff8b94','#ff7480']
-  const roundness = ["0%","5%","10%","20","25%"]
+  const colors = [
+    [
+      "#386641",
+      "#287271",
+      "#2a9d8f",
+      "#8ab17d",
+      "#e9c46a",
+      "#f4a261",
+      "#e76f51",
+    ],
+    [
+      "#f0ead2",
+      "#dde5b6",
+      "#adc178",
+      "#7b8f4b",
+      "#a98467",
+      "#6c584c",
+      "#79675c",
+    ],
+    [      
+    "#5a6776",
+    "#766a76",
+    "#916e75",
+    "#ad7175",
+    "#c87475",
+    "#e47874",
+    "#ff7b74",]
+  ];
+  const roundness = ["0%", "5%", "30", "50%"];
+  
   let rdnindex = 0;
   setInterval(() => {
-      let i = 0;
-      console.log("reload...")
-      let left = 0;
-      let top = 0;
-      let height = 0;
-      let width = 0;
+    let i = 0;
+    console.log("reload...");
+    let left = 0;
+    let top = 0;
+    let height = 0;
+    let width = 0;
 
+    let left0 = 0;
+    let top1 = 0;
+    let rnd = rdnindex;
+    while (rnd === rdnindex) {
+      rnd = Math.floor(Math.random() * 4);
+    }
+    rdnindex = rnd;
+    const indexColors = Math.floor(Math.random() * 2);
+    const indexa = [0, 1, 2, 3, 4, 5, 6];
+    indexa.sort(() => Math.random() - 0.5);
 
-      let left0 = 0;
-      let top1 = 0;
-      let rnd = rdnindex;
-      while(rnd === rdnindex){
-        rnd = Math.floor(Math.random()*4);
-      }
-      rdnindex = rnd;
-
-      const indexa = [0,1,2,3,4,5,6]
-      indexa.sort(() => Math.random() - 0.5);
-
-      while(indexa.length >0){
-        if(i === 0){
-          left = Math.floor(Math.random() * 20) + 45;
-          top = 0;
-          height = 100;
-          width = 100-left;
-          left0 = left
-        }
-
-        else{
-          if( 0<i && i<5 ){
-            if(i===1)
-            {
-              height = Math.floor(Math.random() * 50) + 25;
-              width = Math.floor(Math.random() * 10) + Math.floor(left0/2);
-              top = 0;
-              left = 0;
-              top1 = height;
-              
-            }
-            if(i===2)
-            {
-              left = width;
-              width = left0-left;
-              height = Math.floor(Math.random() * 50) + 25;
-              top = 0;
-
-            }
-            if(i===3)
-            {
-              top=height
-              height = 100-top;
-
-            }
-            if(i===4)
-            {
-              
-              width = left;
-              top=top1;
-              left=0;
-              height= 100-top;
-
-            }
-
+    while (indexa.length > 0) {
+      if (i === 0) {
+        left = Math.floor(Math.random() * 20) + 45;
+        top = 0;
+        height = 100;
+        width = 100 - left;
+        left0 = left;
+      } else {
+        if (0 < i && i < 5) {
+          if (i === 1) {
+            height = Math.floor(Math.random() * 50) + 25;
+            width = Math.floor(Math.random() * 10) + Math.floor(left0 / 2);
+            top = 0;
+            left = 0;
+            top1 = height;
           }
-          else{
-            top = Math.floor(Math.random()  * 101);
-            left = top/1.618;
-            console.log()
-            height = Math.min(Math.floor(Math.random() * 101-top),20);
-            width = height/1.618;
-            
+          if (i === 2) {
+            left = width;
+            width = left0 - left;
+            height = Math.floor(Math.random() * 50) + 25;
+            top = 0;
           }
-          
-
+          if (i === 3) {
+            top = height;
+            height = 100 - top;
+          }
+          if (i === 4) {
+            width = left;
+            top = top1;
+            left = 0;
+            height = 100 - top;
+          }
+        } else {
+          top = Math.floor(Math.random() * 90);
+          left = top / 1.618;
+          console.log();
+          height = Math.max(
+            Math.min(Math.floor(Math.random() * (101 - top)), 40),
+            10
+          );
+          width = height / 1.618;
         }
-        const index = indexa.pop()
-        
-        shapes[i].setAttribute("style", `width:${String(width)}%; top:${String(top)}%; left:${String(left)}%;height:${height}%; background-color:${colors[index]}; border-radius:${roundness[rnd]}  `)
-        i += 1;
       }
+      const index = indexa.pop();
+
+      shapes[i].setAttribute(
+        "style",
+        `width:${String(width)}%; top:${String(top)}%; left:${String(
+          left
+        )}%;height:${height}%; background-color:${
+          colors[indexColors][index]
+        }; border-radius:${roundness[rnd]}  `
+      );
+      i += 1;
+    }
   }, 5000);
   return (
     <div className="App">
